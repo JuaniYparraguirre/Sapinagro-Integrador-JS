35e38651c8a3233572deb5abe6b50d1c89cb77ca
Juani <juaniyparraguirre9@gmaial.com>
2023-10-21 19:02:55 -0300

    3puestaApunto

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// const menuIcon = document.querySelector(".ham");
-// const navList = document.querySelector(".navbar");
-
-// menuIcon.addEventListener("click", () => {
-//   if (navList.style.display === "none" || navList.style.display === "") {
-//     navList.style.display = "block";
-//   } else {
-//     navList.style.display = "none";
-//   }
-// });
-
 const menuIcon = document.querySelector(".ham");
 const navList = document.querySelector(".navbar");
 
@@ -18,10 +7,4 @@ menuIcon.addEventListener("click", () => {
   } else {
     navList.style.display = "none";
   }
-
-  // Agregar o quitar la clase de transición después de cambiar el estilo
-  navList.classList.add("menu-transition");
-  setTimeout(() => {
-    navList.classList.remove("menu-transition");
-  }, 5000); // Esto debe coincidir con la duración de la transición en milisegundos
 });
