0e75ebf07e5936517d19df1db8854b7a77220cd5
Juani <juaniyparraguirre9@gmaial.com>
2023-11-08 00:35:23 -0300

    editantoMediaCueris

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-// FUNCION PARA ABRIR EL MENU HAMBURGUEZA//
-// const menuIcon = document.querySelector(".ham");
-// const navList = document.querySelector(".navbar");
+//FUNCION PARA ABRIR EL MENU HAMBURGUEZA//
+const menuIcon = document.querySelector(".ham");
+const navList = document.querySelector(".navbar");
 
-// menuIcon.addEventListener("click", () => {
-//   if (navList.style.display === "none" || navList.style.display === "") {
-//     navList.style.display = "block";
-//   } else {
-//     navList.style.display = "none";
-//   }
-// });
+menuIcon.addEventListener("click", () => {
+  if (navList.style.display === "none" || navList.style.display === "") {
+    navList.style.display = "block";
+  } else {
+    navList.style.display = "none";
+  }
+});
 
 // Contenedor de productos
 const productsContainer = document.querySelector(".products-container");
